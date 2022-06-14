7ec0738f0d9ebf5fd1a1bebdb31d4f4b8f989e18
mohammad alameri <onex911@hotmail.com>
2022-06-14 16:43:24 +0200

    unit tests

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,26 +2,71 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
     let result;
-    
+    if(input.match(/[\d]+.?[\d]*\/?[\d]*.?[\d]*/)){
+    result=input.match(/[\d]+.?[\d]*\/?[\d]*.?[\d]*/)[0];
+    console.log(result)
+    if(result.includes("/")){
+      let segments=result.split("/");
+      if(segments.length>2){
+        return new Error("double-fraction")
+      }
+      result=segments[0]/segments[1]
+    }
     return result;
+  }else{
+    return 1;
+  }
   };
   
   this.getUnit = function(input) {
     let result;
-    
-    return result;
-  };
+    if(input.match(/gal|L|mi|km|lbs|kg/))
+    {
+      result=input.match(/gal|L|mi|km|lbs|kg/)[0]
+      return result;
+    }else{
+      return new Error("invalid input unit")
+    }
+  }
   
   this.getReturnUnit = function(initUnit) {
-    let result;
+    switch (initUnit) {
+      case "gal":
+        return "L";
+      case "L":
+        return "gal";
+      case "mi":
+        return "km";
+      case "km":
+        return "mi";
+      case "lbs":
+        return "kg";
+      case "kg":
+        return "lbs";
     
-    return result;
+      default:
+        return new Error("invalid unit")
+    }
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
+    switch (unit) {
+      case "gal":
+        return "Gallon";
+      case "L":
+        return "Liter";
+      case "mi":
+        return "Mile";
+      case "km":
+        return "Kilometer";
+      case "lbs":
+        return "Pound";
+      case "kg":
+        return "Kilogram";
     
-    return result;
+      default:
+        return new Error("invalid unit")
+    }
   };
   
   this.convert = function(initNum, initUnit) {
